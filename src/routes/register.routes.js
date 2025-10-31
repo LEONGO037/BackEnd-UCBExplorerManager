45169e20384eb3d228da45aa50254c4c2d52b292
Leonardo Delgado Medrano <leonardo.delgado.medrano@gmail.com>
2025-10-31 16:13:10 -0400

    olivde mi contrasenia

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -7,6 +7,8 @@ router.post('/register', RegisterController.register);
 router.post('/change-password', RegisterController.changePassword);
 router.get('/has-logged-before', RegisterController.hasLoggedInBefore);
 router.post('/verify-email', RegisterController.verifyEmail);
+router.post('/verify-code', RegisterController.verifyCode);
+router.post('/update-password', RegisterController.updatePasswordByEmail);
 
 
 export default router;
