4e49d86e84d442a6bb2ecd8253ce9ca74893b62a
Leonardo Delgado Medrano <leonardo.delgado.medrano@gmail.com>
2025-10-31 12:34:20 -0400

    First login

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/register', RegisterController.register);
 router.post('/change-password', RegisterController.changePassword);
+router.get('/has-logged-before', RegisterController.hasLoggedInBefore);
 
 export default router;
