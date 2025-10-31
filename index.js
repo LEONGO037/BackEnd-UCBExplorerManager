57b7b556a22523f9a0abe38ba230f590ee7c20e4
ChristianCoronel404 <142526533+ChristianCoronel404@users.noreply.github.com>
2025-10-31 02:36:21 -0400

    Implementación de los end points de logs
    Se añadio el controlador, modelo y rutas de los logs, para su respectivo uso en el front end

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import logsRoutes from "./src/routes/logs.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/logs", logsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Servidor activo con watch ✅");
